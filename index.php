46c520965be1e6543126bc730f22434d49927d77
Hecker-Frank <110228046+Frankenstien101@users.noreply.github.com>
2025-08-19 23:21:54 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         body {
             color: var(--text);
             min-height: 100vh;
-            background-image: url('MainImg/vertudo_system-hintergrund_2880x1952-1024x694.jpg');
+            background-image: url('MainImg/44.jpg');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
