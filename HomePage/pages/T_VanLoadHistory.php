0fe02dd8b7c1100950f61795b627236197817bae
Hecker-Frank <110228046+Frankenstien101@users.noreply.github.com>
2025-08-24 17:20:12 +0800

    Add files via upload

--- HomePage/pages/T_VanLoadHistory.php
+++ HomePage/pages/T_VanLoadHistory.php
@@ -163,7 +163,7 @@
     </div>
 
     <!-- Date Filter Card -->
-    <div class="card text-bg-light" style="max-width: 50%; font-size: 9px;">
+    <div class="card text-bg-light" style="max-width: 100%; font-size: 9px;">
         <div class="card-header d-flex justify-content-between align-items-center">
             <span>SELECT DATE FILTER</span>
         </div>
@@ -197,23 +197,23 @@
 
   <label class="radio-label mr-1">
     <input type="radio" name="status" value="PENDING">
-    <span class="radio-custom bg-blue">PENDING</span>
+    <span class="radio-custom bg-blue">| PENDING |</span>
   </label>
 
 
   <label class="radio-label mr-1">
     <input type="radio" name="status" value="ALLOCATED">
-    <span class="radio-custom bg-gold">ALLOCATED</span>
+    <span class="radio-custom bg-gold">| ALLOCATED |</span>
   </label>
   
   <label class="radio-label mr-1">
     <input type="radio" name="status" value="LOCKED">
-    <span class="radio-custom bg-red">LOCKED</span>
+    <span class="radio-custom bg-red">| LOCKED |</span>
   </label>
 
     <label class="radio-label mr-1">
     <input type="radio" name="status" value="APPROVED">
-    <span class="radio-custom bg-green">APPROVED</span>
+    <span class="radio-custom bg-green">| APPROVED |</span>
   </label>
   
 
