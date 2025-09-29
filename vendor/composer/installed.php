657b7f35a0b8630bc6687f49ad33d0762083bda9
Frankenstien101 <frankpadios.bspi@gmail.com>
2025-09-29 20:14:01 +0800

    Include vendor folder for Azure deployment

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b49848a8ab6b7a4acc5081de49ff36a25b5fca52',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b49848a8ab6b7a4acc5081de49ff36a25b5fca52',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
