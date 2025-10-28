b1350a3b4ca6ec945e480a98423f1e406d768dfc
Geonx14 <jenerlaureano.bspi@gmail.com>
2025-10-28 17:31:34 +0800

    sample commit

--- pqr_test/index.php
+++ pqr_test/index.php
@@ -14,7 +14,6 @@ $condition = ($role != 'Admin') ? " AND USER_ID=$user_id" : "";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
